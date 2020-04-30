aa0bf56bc244c9fbbb3582da9e0b84af4efa9aab
Denis Onu <deni_onu@yahoo.com>
2020-04-30 18:21:07 +0300

    I cannot memorize urls so I pasted them in the App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import All from './components/All';
 
+
+// all cases api:https://coronavirus-19-api.herokuapp.com/all
+// country cases:https://coronavirus-19-api.herokuapp.com/countries
+
 function App() {
   return (
     <React.Fragment>

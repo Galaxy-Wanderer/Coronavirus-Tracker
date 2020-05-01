2d62c11202f0cb5c1ea000cac76cade851829032
Denis Onu <deni_onu@yahoo.com>
2020-05-01 14:56:49 +0300

    Working on search filter

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Search = () => {
     const [value, setValue] = useState('');
-    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState('')
     const submitForm = (e) => {
         e.preventDefault();
-        console.log(value);
+        setQuery(value);
     }
     const changeInput = (e) => {
         setValue(e.target.value);
     }
+    useEffect(() => {
+        const fetchCountries = async () => {
+            const res = await fetch('https://coronavirus-19-api.herokuapp.com/countries');
+            const data = await res.json();
+            data.forEach(place => {
+                if(place.country.toUpperCase().indexOf(query.toUpperCase()) > -1) {
+                    setResults(place);
+                }
+            });
+        }
+        fetchCountries();
+    }, [query]);
     return (
         <div className='search'>
             <h2>Search by country</h2>
@@ -17,6 +30,10 @@ const Search = () => {
                 <input type="text" autoComplete='off' required placeholder='Enter country...' value={value} onChange={changeInput} />
                 <button type='submit'><img src="/search.png" alt="search icon"/></button>
             </form>
+            <ul>
+                {results.country}
+                {results.cases}
+            </ul>
         </div>
     )
 }
